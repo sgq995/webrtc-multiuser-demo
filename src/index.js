777fbf9b3c5d5553da2bb31186d843b161621122
Sebastian G. Q <sgq995@gmail.com>
2020-06-16 12:03:12 -0500

    feat: Added creator for WebRTC peer connection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const PEERS = new Map();
+const VIEWS = new Map();
+
 async function getDefaultStream() {
     try {
         const devices = await navigator.mediaDevices.enumerateDevices();
@@ -12,6 +15,46 @@ async function getDefaultStream() {
         console.error(error);
         return null;
     }
+}
+
+function createPeerConnection(id) {
+    const peerConnection = new RTCPeerConnection({
+        iceServers: [{
+            urls: [
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302',
+                'stun:stun3.l.google.com:19302',
+                'stun:stun4.l.google.com:19302',
+            ]
+        }]
+    });
+    peerConnection.onicecandidate = ({ candidate }) => signalingSend(JSON.stringify({ candidate }));
+    peerConnection.onnegotiationneeded = async () => {
+        try {
+            await peerConnection.setLocalDescription(await peerConnection.createOffer());
+            signalingSend(JSON.stringify({ description: peerConnection.localDescription }));
+        } catch (err) {
+            console.error(err);
+        }
+    };
+    peerConnection.on
+
+    const remoteView = document.createElement('video');
+    remoteView.autoplay = true;
+    remoteView.id = id;
+
+    peerConnection.ontrack = (event) => {
+        if (remoteView.srcObject) return;
+
+        const stream = event.streams[0];
+        remoteView.srcObject = stream;
+    };
+
+    document.body.appendChild(remoteView);
+
+    VIEWS.set(id, remoteView);
+    PEERS.set(id, peerConnection);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
