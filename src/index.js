c306cc1f5e0844108843110ad0ef6ef61aa15847
Sebastian G. Q <sgq995@gmail.com>
2020-06-18 11:44:23 -0500

    feat: Added signaling client & server
    feat: Added default size to remote videos
    
    feat: Added RTC closed detection
    
    fix: Added target's id to some signaling messages

--- src/index.js
+++ src/index.js
@@ -1,23 +1,86 @@
-const PEERS = new Map();
-const VIEWS = new Map();
+const peerList = new Map();
+let signaling = null;
 
 async function getDefaultStream() {
     try {
         const devices = await navigator.mediaDevices.enumerateDevices();
-    
+
         const constrains = { video: false, audio: false };
         constrains.video = devices.some(device => device.kind === 'videoinput');
         constrains.audio = devices.some(device => device.kind === 'audioinput');
-    
+
         const stream = await navigator.mediaDevices.getUserMedia(constrains);
         return stream;
-    } catch {
+    } catch (error) {
         console.error(error);
         return null;
     }
 }
 
+function signalingSend(object) {
+    const data = JSON.stringify(object);
+    console.log('[SEND] ', data);
+    signaling.send(data);
+}
+
+async function signalingStart(stream) {
+    signaling = new WebSocket('wss://' + location.host + '/signaling');
+    signaling.onopen = () => {
+        signalingSend({ type: 'join' });
+    }
+    signaling.onmessage = async (message) => {
+        console.log('[RECEIVE] ', message.data);
+        const { type, source, peerList, description, candidate } = JSON.parse(message.data);
+
+        try {
+            if (type === "list") {
+                peerList.forEach(async (peerId) => {
+                    const peerConnection = createPeerConnection(peerId);
+                    
+                    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+                    const localDescription = await peerConnection.createOffer({
+                        offerToReceiveAudio: true,
+                        offerToReceiveVideo: true
+                    });
+                    await peerConnection.setLocalDescription(localDescription);
+                    signalingSend({ target: peerId, description: peerConnection.localDescription });
+                });
+            } else if (description) {
+                const peerConnection = createPeerConnection(source);
+
+                if (description.type === 'offer') {
+                    await peerConnection.setRemoteDescription(description);
+
+                    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+                    const localDescription = await peerConnection.createAnswer({
+                        offerToReceiveAudio: true,
+                        offerToReceiveVideo: true
+                    });
+                    await peerConnection.setLocalDescription(localDescription);
+                    signalingSend({ target: source, description: peerConnection.localDescription });
+                } else if (description.type === 'answer') {
+                    await peerConnection.setRemoteDescription(description);
+                } else {
+                    console.error('Unsuported SDP type: ', description);
+                }
+            } else if (candidate) {
+                const peerConnection = createPeerConnection(source);
+
+                await peerConnection.addIceCandidate(candidate);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
 function createPeerConnection(id) {
+    if (peerList.has(id)) {
+        return peerList.get(id);
+    }
+
     const peerConnection = new RTCPeerConnection({
         iceServers: [{
             urls: [
@@ -26,35 +89,71 @@ function createPeerConnection(id) {
                 'stun:stun2.l.google.com:19302',
                 'stun:stun3.l.google.com:19302',
                 'stun:stun4.l.google.com:19302',
+                'stun:stun01.sipphone.com',
+                'stun:stun.ekiga.net',
+                'stun:stun.fwdnet.net',
+                'stun:stun.ideasip.com',
+                'stun:stun.iptel.org',
+                'stun:stun.rixtelecom.se',
+                'stun:stun.schlund.de',
+                'stun:stunserver.org',
+                'stun:stun.softjoys.com',
+                'stun:stun.voiparound.com',
+                'stun:stun.voipbuster.com',
+                'stun:stun.voipstunt.com',
+                'stun:stun.voxgratia.org',
+                'stun:stun.xten.com',
             ]
         }]
     });
-    peerConnection.onicecandidate = ({ candidate }) => signalingSend(JSON.stringify({ candidate }));
+    peerConnection.onicecandidate = ({ candidate }) => signalingSend({ target: id, candidate });
     peerConnection.onnegotiationneeded = async () => {
         try {
-            await peerConnection.setLocalDescription(await peerConnection.createOffer());
-            signalingSend(JSON.stringify({ description: peerConnection.localDescription }));
-        } catch (err) {
-            console.error(err);
+            await peerConnection.setLocalDescription(await peerConnection.createOffer({
+                offerToReceiveAudio: true,
+                offerToReceiveVideo: true
+            }));
+            signalingSend({ target: id, description: peerConnection.localDescription });
+        } catch (error) {
+            console.error(error);
         }
     };
-    peerConnection.on
 
     const remoteView = document.createElement('video');
+    remoteView.width = 240;
+    remoteView.height = 320;
     remoteView.autoplay = true;
     remoteView.id = id;
 
+    peerConnection.oniceconnectionstatechange = (event) => {
+        switch (peerConnection.iceConnectionState) {
+            case "disconnected":
+            case "failed":
+                console.log("One or more transports has terminated unexpectedly");
+            case "closed":
+                peerConnection.close();
+                console.log("Connection has been closed");
+                let view = document.getElementById(id);
+                console.log(view);
+                if (view) {
+                    view.remove();
+                }
+                peerList.delete(id);
+                break;
+        }
+    };
     peerConnection.ontrack = (event) => {
-        if (remoteView.srcObject) return;
-
         const stream = event.streams[0];
+
+        remoteView.srcObject = null;
         remoteView.srcObject = stream;
     };
 
     document.body.appendChild(remoteView);
 
-    VIEWS.set(id, remoteView);
-    PEERS.set(id, peerConnection);
+    peerList.set(id, peerConnection);
+
+    return peerConnection;
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -62,4 +161,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     const localView = document.getElementById('local-view');
     localView.srcObject = stream;
+
+    signalingStart(stream);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,98 @@
 const path = require('path');
-const fs = require('fs');
+const crypto = require('crypto');
 const WebSocket = require('ws');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const e = require('express');
+
+const userList = new Map();
+
+function generateId() {
+    let id;
+    do {
+        id = crypto.randomBytes(16).toString("hex");
+    } while (userList.has(id));
+
+    return id;
+}
+
+function sendToClient(socket, object) {
+    const data = JSON.stringify(object);
+    console.log('[SERVER] ', data);
+    socket.send(data);
+}
+
+function createOnMessage(id, socket) {
+    const onMessage = (data) => {
+        console.log('[CLIENT] ', id, data);
+        const object = JSON.parse(data);
+        const { type, target, description, candidate } = object;
+
+        try {
+            if (type === "join") {
+                const peerList = Array.from(userList.keys()).filter(otherId => otherId !== id);
+                sendToClient(socket, {
+                    type: "list",
+                    peerList
+                });
+                userList.set(id, socket);
+            } else if (description !== undefined) {
+                if (userList.has(target)) {
+                    const peerSocket = userList.get(target);
+                    sendToClient(peerSocket, {
+                        source: id,
+                        description
+                    });
+                }
+            } else if (candidate !== undefined) {
+                if (userList.has(target)) {
+                    const peerSocket = userList.get(target);
+                    sendToClient(peerSocket, {
+                        source: id,
+                        candidate
+                    });
+                }
+            }
+        } catch {
+
+        }
+    };
+
+    return onMessage;
+}
+
+function createOnClose(id, socket) {
+    const onClose = (code, reason) => {
+        console.log('[CLOSE] ', id, code, reason);
+        userList.delete(id);
+    };
+
+    return onClose;
+}
+
+function createOnError(id, socket) {
+    const onError = (err) => {
+        console.error('[ERROR] ', id, err);
+        userList.delete(id);
+        this.close();
+    };
+
+    return onError;
+}
+
+function createWebSocketServer() {
+    const wss = new WebSocket.Server({
+        port: 3000
+    });
+
+    wss.on('connection', (socket) => {
+        const id = generateId();
+
+        socket.on('message', createOnMessage(id, socket));
+        socket.on('close', createOnClose(id, socket));
+        socket.on('error', createOnError(id, socket));
+    });
+}
 
 module.exports = {
     mode: 'development',
@@ -23,17 +113,8 @@ module.exports = {
                 ws: true,
             },
         },
-        after: function(app, server, compiler) {
-            const wss = new WebSocket.Server({
-                port: 3000
-            });
-
-            wss.on('connection', (socket) => {
-                socket.on('message', (message) => {
-                    console.log('received %s', message);
-                    socket.send(message);
-                });
-            });
+        after: function (app, server, compiler) {
+            createWebSocketServer();
         }
     },
     plugins: [
